b5efde6c1376b7336c321ee922039c22a780bd94
339780520 <339780520@2260I-L29-008>
2025-10-02 09:14:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T1_6_Summative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
