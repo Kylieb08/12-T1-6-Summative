a6dca85a005d6d824f07428a626f649e6675e08e
339780520 <339780520@2260I-L29-008>
2025-10-03 09:33:37 -0400

    finished doubles

--- Program.cs
+++ Program.cs
@@ -8,8 +8,90 @@
             Die die1, die2;
             bool done = false;
             double balance = 100, bet;
+            string outcomeGuess;
 
-            Console.WriteLine("Hello, World!");            
+            die1 = new Die();
+            die2 = new Die();
+
+            Console.WriteLine("Welcome to the casino!");
+            Console.WriteLine("You have 100$ in your account to start.");
+            Console.WriteLine("You can use that money to bet on the outcome of a dice roll.");
+            Console.WriteLine("If you bet more than your balance, it will be assumed that you are all in");
+            Console.WriteLine("The house will roll two dice, and you will bet on whether you think");
+            Console.WriteLine("it will be doubles, not doubles, an odd sum, or an even sum.");
+            Console.WriteLine("Each option will win you different amounts of money.");
+            Console.WriteLine();
+            Console.WriteLine("Betting on doubles will win you twice your bet");
+            Console.WriteLine("Betting on not doubles will win you half your bet");
+            Console.WriteLine("Betting on an odd sum or an even sum will win you exactly your bet");
+            Console.WriteLine("You can also choose to quit when prompted for which outcome you would like to bet on");
+
+            while (!done)
+            {
+                if (balance <= 0)
+                {
+                    Console.WriteLine("You have run out of funds");
+                    Console.WriteLine("Game Over");
+                    done = true;
+                }
+
+                else
+                {
+                    die1.RollDie();
+                    die2.RollDie();
+                    Console.WriteLine("What do you think the outcome will be? (please enter the name, not the number)");
+                    Console.WriteLine();
+                    Console.WriteLine("1. Doubles");
+                    Console.WriteLine("2. Not Doubles");
+                    Console.WriteLine("3. Odd Sum");
+                    Console.WriteLine("4. Even Sum");
+                    Console.WriteLine("5. Quit");
+
+                    outcomeGuess = Console.ReadLine().ToLower();
+
+                    switch (outcomeGuess)
+                    {
+                        case "doubles":
+                            Console.Clear();
+                            Console.WriteLine("You bet on doubles");
+                            Console.WriteLine("How much would you like to bet?");
+
+                            while (!double.TryParse(Console.ReadLine(), out bet))
+                            {
+                                Console.WriteLine("That is not a valid number");
+                                Console.WriteLine("Please try again");
+                            }
+
+                            if (bet > balance)
+                            {
+                                Console.WriteLine("You bet more than your balance");
+                                Console.WriteLine("You are all in");
+                                bet = balance;
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine($"You bet {bet} dollars");
+                            die1.DrawRoll();
+                            die2.DrawRoll();
+
+                            if (die1.Roll == die2.Roll)
+                            {
+                                Console.WriteLine("Congratulations! You win");
+                                balance += (bet * 2);
+                                Console.WriteLine($"You have {balance} dollars in your account");
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("You lost");
+                                balance -= (bet * 2);
+                                Console.WriteLine($"You have {balance} dollars in your account");
+                            }
+
+                                break;
+                    }
+                }
+            }
         }
     }
 }
